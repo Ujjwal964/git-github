ae4500d356d4636372a6af0f5786089081592a83
Ujjwal Bansal <ujjbansal964@gmail.com>
2026-01-24 14:30:06 +0530

    added above18 func

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const age = 22;
-const name = "Ujjwal Bansal"
+const name1 = "Ujjwal Bansal"
 const dob = "20-03-2003"
 
 function name(){
-    console.log(`My name is ${name}`)
+    console.log(`My name is ${name1}`)
+}
+
+function isAbove18(){
+    return age>=18
 }
